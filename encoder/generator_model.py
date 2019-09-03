95cec2040f6bc5de727968b157b730148cbd2bf7
Uha661 <54659541+Uha661@users.noreply.github.com>
2019-09-03 17:27:24 +0530

    Update generator_model.py

--- encoder/generator_model.py
+++ encoder/generator_model.py
@@ -29,8 +29,7 @@ class Generator:
         self.sess = tf.get_default_session()
         self.graph = tf.get_default_graph()
         try:
-            while True:
-                self.dlatent_variable = next(v for v in tf.global_variables() if 'learnable_dlatents' in v.name,1)
+            self.dlatent_variable = next(v for v in tf.global_variables() if 'learnable_dlatents' in v.name,1)
         except StopIteration:
             pass
         self.set_dlatents(self.initial_dlatents)
