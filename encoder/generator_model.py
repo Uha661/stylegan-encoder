29413f6293eb4154d5f64c3b8da1187751e25dba
Uha661 <54659541+Uha661@users.noreply.github.com>
2019-09-04 10:35:18 +0530

    Update generator_model.py

--- encoder/generator_model.py
+++ encoder/generator_model.py
@@ -28,10 +28,10 @@ class Generator:
 
         self.sess = tf.get_default_session()
         self.graph = tf.get_default_graph()
-        try:
-            self.dlatent_variable = next(v for v in tf.global_variables() if 'learnable_dlatents' in v.name,1)
-        except StopIteration:
-            pass
+        #try:
+        self.dlatent_variable = next(v for v in tf.global_variables() if 'learnable_dlatents' in v.name,1)
+        #except StopIteration:
+        #    pass
         self.set_dlatents(self.initial_dlatents)
 
         self.generator_output = self.graph.get_tensor_by_name('G_synthesis_1/_Run/concat:0')
